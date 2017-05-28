8413b9bd02fc28f9d4ee7cb8da571ad37498cade
DharaVihol <dharavihol119@gmail.com>
2017-05-28 18:45:52 +0530

    Fixes #163: Resolved the problem of refilling of data after the duplicate email verification failed

--- registration.php
+++ registration.php
@@ -1,16 +1,12 @@
 <!--Created by Akanksha
     Desc: Registration form and code to register the user
 -->
-<?php
-  require_once('includes/settings.php');
-?>
 <!DOCTYPE html>
 <html>
 <head>
   <title>FirstAide</title>
   <link rel="stylesheet" type="text/css" href="css files/loginAndRegistration.css">
   <link rel="stylesheet" href="css files/sweetalert.css">
-  <script src="https://www.google.com/recaptcha/api.js" async defer></script>
   <form method="POST" onsubmit="return validate()" /><!--validations for form fields entered js file- validation-v12.js-->
 </head>
 <body>
@@ -25,7 +21,7 @@
     <table class="tables">
        <tr>
          <th class="text">Username:</th>
-         <td><input class="input-box" type="text" id="uname" name="uname" placeholder="Enter your username" required/></td>
+         <td><input class="input-box" type="text" id="uname" name="uname" placeholder="xyz123" maxlength="25" required/></td>
        </tr>
         <tr>
          <th class="text" style="vertical-align: top">Password:</th>
@@ -43,50 +39,29 @@
            </div>
 
          </td>
-         </tr>
-        <tr>
-         <th class="text" style="vertical-align: top">Confirm Password:</th>
-         <td>
-           <input class="input-box" type="password" id="confirmpassword" name="confirmpassword" required/>
-
-         </td>
         <tr>
          <th class="text">Host Country:</th>
-         <td><select id = "host_country" name = "host_country" required></select></td>
+         <td><input class="input-box" type="text" id="host_country" name="host_country" placeholder="India" required/></td>
        </tr>
        <tr>
          <th class="text">Email:</th>
-         <td><input class="input-box" type="text" id="email" name="email" placeholder="Enter your email address" required /></td>
+         <td><input class="input-box" type="text" id="email" name="email" placeholder="xyz@gmail.xom" required /></td>
        </tr>
     </table>
-    <div class="g-recaptcha" data-sitekey="<?php echo $_settings['reCaptcha']['client_key']?>"></div> 
   </div>
   <!--submit button-->
   <div class="div-reg">
+     <input class="button" type="submit" id="submit" value="Sign in to Account" onclick="location.href='login.php';">
      <input class="button" type="submit" value="Create Account">
     <br><br>
-    <p class="text">Already have an account?&nbsp<a href ="login.php">Sign in</a></p><br><br>  
     <img src="images/secure.png" style="width: 20px; height: 20px;"/>
-    <div class="text">This is a secure portal</div>
+    <a href="#">This is a secure portal</a>
   </div>
 </center>
   <script src="javascripts/passwordStrengthChecker.js"></script>
   <script type="text/javascript" src="javascripts/validation-v12.js"></script>
   <script src="javascripts/sweetalert.min.js"></script>
   <script src="javascripts/sweetalert.js"></script>
-  <script src="javascripts/jquery-1.12.4.min.js"></script>
-  <script> 
-  $.getJSON("javascripts/country.json", function(json) {
-    
-    var countryElement = $("#host_country");
-    $.each(json,function(index, value){
-    var countryOption = '<option value="' + value + '">'+ value + '</option>';
-    if(value == 'Uganda') {
-       countryOption = '<option value="' + value + '" selected>'+ value + '</option>';
-    }
-    countryElement.append(countryOption);
-  });
-  });</script>
 </body>
 </html>
 <?php
@@ -100,38 +75,25 @@
 
    require 'dbconnect.php';
 
-   if(isset($_POST['email'])&&isset($_POST['uname'])&&isset($_POST['password'])&&isset($_POST['host_country'])&&!empty($_POST['email'])&&!empty($_POST['uname']))
+   if(isset($_POST['email'])&&isset($_POST['uname'])&&isset($_POST['password'])&&isset($_POST['host_country']))
    {
-     $reCaptchaUrl = 'https://www.google.com/recaptcha/api/siteverify';
-     $ch = curl_init();
-
-     curl_setopt($ch, CURLOPT_URL, $reCaptchaUrl);
-     curl_setopt($ch, CURLOPT_POST, 1);
-     curl_setopt($ch, CURLOPT_POSTFIELDS, "secret=".$_settings['reCaptcha']['server_key']."&response=".$_POST['g-recaptcha-response']);
-     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-     $server_output = curl_exec ($ch);
-     curl_close ($ch);
-     $reCaptchaResponse = json_decode($server_output);
-     if(!$reCaptchaResponse->success)
-     {
-        echo "<script type='text/javascript'>salert('Oops','Captcha not verified','error');</script>";
-     }
-     else
-     {
       $sql="CALL dupemail('$_POST[email]')";
       $result = mysqli_query($connection,$sql);
       $connection -> next_result(); //used when there are multiple procedure calls, use after ecah procedure call
 
       if(mysqli_num_rows($result)>=1) //check if it is a duplicate email
-      {
-         echo "<script type='text/javascript'>salert('Oops','User with this email already exists','error');</script>";
-      }
+      { ?>
+        <script type='text/javascript'>
+                document.getElementById('uname').value = '<?php echo $_POST['uname'];?>';
+                document.getElementById('password').value = '<?php echo $_POST['password'];?>';
+                document.getElementById('host_country').value = '<?php echo $_POST['host_country'];?>';
+                salert('Oops','User with this email already exists','error');</script>
+         
+      <?php }
       else
       {
         $email = $_POST['email'];
-        $password = $_POST['password'];
-        $password = md5($email.$password);
-        $newUser="CALL registration('$_POST[email]','$_POST[uname]','$password','$_POST[host_country]')"; //inserts into the user table
+        $newUser="CALL registration('$_POST[email]','$_POST[uname]','$_POST[password]','$_POST[host_country]')"; //inserts into the user table
 
         if(mysqli_query($connection,$newUser))
         {//if successfully added user then add comrades of user with null phone numbers
@@ -151,6 +113,6 @@
           echo "<script type='text/javascript'>salert('Oops','Error in adding user','error');</script>";
       }
       mysqli_close($connection);
-     }
-    }
+   }
+
 ?>
